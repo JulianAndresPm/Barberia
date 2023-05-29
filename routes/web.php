60866dd855cb0b2dd003163e7cb23d34af931c0a
Luis Rosero <luiscrt2002@gmail.com>
2023-05-28 19:13:20 -0500

    Feat: cambios luis

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,8 @@ Route::get('/servicios','ProductosController@servicios');
 
 
 // Rutas de vistas 5 y 6
-Route::get('/vista5','ClientesController@login');
-Route::get('/vista6','ClientesController@formulario');
+Route::get('/login','ClientesController@login');
+Route::get('/formulario','ClientesController@formulario');
 
 
 
